97ed6ae457bc534b809ec5642a2d99bfbef07817
Shahidul Islam Majumder <dev@shahid.pro>
2020-04-17 13:15:17 +0600

    :arrow_up_down: Update packages to fix vulnerabilities

--- src/views/components/SEO.js
+++ src/views/components/SEO.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Helmet from 'react-helmet';
+import {Helmet} from 'react-helmet';
 import urljoin from 'url-join';
 import config from '../../../data/SiteConfig';
 
