4426d53208d69139fa63df3ede9a3830345f3a7d
Shahidul Islam Majumder <dev@shahid.pro>
2020-06-06 19:55:04 +0600

    :heavy_plus_sign: Add back to top button on post page

--- src/views/layouts/ArticleLayout.js
+++ src/views/layouts/ArticleLayout.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Box } from '../components';
+import { Box, ScrollToTopButton } from '../components';
 
 const ArticleLayout = ({ children, ...props }) => (
   <Box maxWidth={960} margin="0 auto" p={{ xs: 3, sm: 4 }} {...props}>
     {children}
+    <ScrollToTopButton />
   </Box>
 );
 
