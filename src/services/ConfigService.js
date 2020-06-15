994580e1cff400bc35cf55e904109c1eeaf1fa52
Shahidul Islam Majumder <dev@shahid.pro>
2020-06-15 11:54:38 +0600

    :bug: Fix scroll to top button hide issue

--- src/services/ConfigService.js
+++ src/services/ConfigService.js
@@ -1,11 +1,13 @@
+import { isBrowser } from '../config';
+
 export const setConfig = (data) => {
-  if (typeof window !== 'undefined') {
+  if (isBrowser) {
     window.localStorage.setItem('config', JSON.stringify(data));
   }
 };
 
 export const getConfig = () => {
-  if (typeof window !== 'undefined') {
+  if (isBrowser) {
     return (
       JSON.parse(window.localStorage.getItem('config')) || {
         theme: { name: 'dark' },
