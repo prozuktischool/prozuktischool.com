666763409f7e746d6fdaa1749145988c79a49ccc
Shahidul Islam Majumder <dev@shahid.pro>
2021-03-04 16:31:18 +0600

    ♻ Refactor image

--- src/views/components/HeroSection.js
+++ src/views/components/HeroSection.js
@@ -1,47 +1,35 @@
 import React from 'react';
-import { graphql, useStaticQuery } from 'gatsby';
 import styled from 'styled-components';
-import BackgroundImage from 'gatsby-background-image';
-import Image from './Image';
 import HeroLogo from '../assets/images/hero-logo.svg';
 
-const HeroContainer = styled(BackgroundImage)`
+const HeroContainer = styled.div`
   min-height: 420px;
   display: flex;
   justify-content: center;
   align-items: center;
   margin-bottom: 32px;
+  background-image: url('/assets/images/hero.jpg');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
   clip-path: polygon(0 0, 100% 0, 100% 50%, 50% 100%, 0 50%);
-
+  svg {
+    height: 25vh;
+    width: 84vw;
+  }
   @media screen and (max-width: 576px) {
-    min-height: 240px;
-    clip-path: polygon(0 0, 100% 0, 100% 70%, 50% 100%, 0 70%);
+    clip-path: polygon(0 0, 100% 0, 100% 50%, 50% 100%, 0 50%);
+    min-height: 220px;
 
     svg {
-      width: 60vw;
+      width: 64vw;
     }
   }
 `;
 
 const HeroSection = () => {
-  const data = useStaticQuery(
-    graphql`
-      query {
-        desktop: file(relativePath: { eq: "hero.jpg" }) {
-          childImageSharp {
-            fluid(quality: 60, maxWidth: 1920) {
-              ...GatsbyImageSharpFluid_noBase64
-            }
-          }
-        }
-      }
-    `
-  );
-
-  const imageData = data.desktop.childImageSharp.fluid;
-
   return (
-    <HeroContainer fluid={imageData}>
+    <HeroContainer>
       <HeroLogo />
     </HeroContainer>
   );
