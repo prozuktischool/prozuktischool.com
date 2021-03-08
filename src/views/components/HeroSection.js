63055e6c0b50603a048e15197fa550edb63b42da
Shahidul Islam Majumder <dev@shahid.pro>
2021-03-08 14:33:09 +0600

    🐛 Fix light theme

--- src/views/components/HeroSection.js
+++ src/views/components/HeroSection.js
@@ -8,18 +8,22 @@ const HeroContainer = styled.div`
   justify-content: center;
   align-items: center;
   margin-bottom: 32px;
-  background-image: linear-gradient(
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  clip-path: polygon(0 0, 100% 0, 100% 50%, 50% 100%, 0 50%);
+
+  background-image: ${({ theme }) =>
+    theme.name === 'dark'
+      ? `linear-gradient(
     to bottom,
     #1c1826,
     #1e1a29,
     #211c2d,
     #231e30,
     #262034
-  );
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
-  clip-path: polygon(0 0, 100% 0, 100% 50%, 50% 100%, 0 50%);
+  )`
+      : `linear-gradient(to top, #e2e4e5, #e7e9ea, #edeeef, #f2f4f5, #f8f9fa)`};
 
   svg {
     position: relative;
